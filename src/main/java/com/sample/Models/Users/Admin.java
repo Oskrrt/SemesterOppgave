b0a22781e542f72a1d2b9af776de4370a49b30bd
SimenRenberg <simenrenberg@gmail.com>
2020-03-28 20:54:43 +0100

    stuff

--- src/main/java/com/sample/Models/Users/Admin.java
+++ src/main/java/com/sample/Models/Users/Admin.java
@@ -1,9 +1,7 @@
 package com.sample.Models.Users;
 
 public class Admin extends User {
-    private boolean hasAdminPowers;
-
     public Admin(boolean hasAdminPowers){
-        this.hasAdminPowers = hasAdminPowers;
+
     }
 }
