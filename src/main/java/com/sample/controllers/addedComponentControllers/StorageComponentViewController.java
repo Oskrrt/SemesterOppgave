93eb1720f82c86ab9554fb69f4e4446c1b341018
SimenRenberg <simenrenberg@gmail.com>
2020-05-12 18:46:48 +0200

    test

--- src/main/java/com/sample/controllers/addedComponentControllers/StorageComponentViewController.java
+++ src/main/java/com/sample/controllers/addedComponentControllers/StorageComponentViewController.java
@@ -51,6 +51,7 @@ public class StorageComponentViewController {
         startThread();
     }
 
+    /*TODO slett denne todo-en */
     //this function uses loads added components in their own thread.
     private void startThread(){
         Thread openStorageComponentFilesThread = new Thread(opener);
